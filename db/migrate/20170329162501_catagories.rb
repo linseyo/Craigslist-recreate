f237bd46b030e9ff573cb8bca7f92d7869a480f3
Austin Kading <email": null>
2017-03-29 16:29:15 -0700

    adds catagory and article migrations

---
+++ db/migrate/20170329162501_catagories.rb
@@ -0,0 +1,8 @@
+class Catagories < ActiveRecord::Migration
+  def change
+    create_table :catagories do |t|
+      t.string :title
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20170329162535_articles.rb
@@ -0,0 +1,11 @@
+class Articles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.integer :price
+      t.string :email
+      t.string :title
+      t.text :description
+      t.timestamps null: false
+    end
+  end
+end

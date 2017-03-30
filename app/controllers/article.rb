453af3ec20b025db75c9dc28d4e4b71ecd715119
Austin Kading <email": null>
2017-03-30 13:52:39 -0700

    adds form to article new page. need to fix routing

--- app/controllers/article.rb
+++ app/controllers/article.rb
@@ -1,9 +1,9 @@
 get '/articles/new/:catagory_id' do
   @catagory = Catagory.find(params[:catagory_id])
-  params[:catagory_id]
   erb :'articles/new'
 end
 
 post '/articles/new/:catagory_id' do
-  @article = Article.new(name: params)
+  @article = Article.create(params['article'])
+  redirect to 'articles/index'
 end

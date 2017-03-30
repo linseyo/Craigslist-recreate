a915d97982cbb3aa71f7314ee6fa91182cd8e8e8
jmxu94 <jmxu94@gmail.com>
2017-03-29 18:50:34 -0700

    Add association between article & category, add validations for article & category models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ActiveRecord::Base
   # Remember to create a migration!
+  belongs_to :category
+
+  validates :name, presence: true
+  validates :category_id, presence: true
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :articles
+
+  validates :name, presence: true
 end

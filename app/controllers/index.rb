25e492c3f0b954fbcf5047bc4133bc3de3ed3bd2
Joshua Mun <email": null>
2017-03-29 18:11:30 -0700

    check point, building get for href to show specific article page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,11 @@
 get '/' do
+  @categories = Category.all
   # Look in app/views/index.erb
-  erb :index
+  erb :'/index'
+end
+
+get '/articles/:category' do
+  @category = params[:category]
+
+  erb :'/articles/index'
 end

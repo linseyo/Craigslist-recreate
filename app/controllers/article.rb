0477a341c09c4fa8981651932ba668c93aa2ac26
Linsey R <email": null>
2017-03-29 18:09:07 -0700

    process- create a new article in a specific category

--- app/controllers/article.rb
+++ app/controllers/article.rb
@@ -2,3 +2,8 @@ get '/articles' do
   @articles = Article.all
   erb :'articles/index'
 end
+
+get '/articles/:article_id' do
+  @article = Article.find(params[:article_id])
+  erb :'articles/show'
+end

bd2d5f65e6d41465ed5909aadedadb2e5232dc4a
joshmun <joshuamun@gmail.com>
2017-03-29 19:58:56 -0700

    Seeded data with categories and articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# require_relative '../models/category'
+require 'faker'
 
 categories = ["auto", "bikes", "books", "cell Phones", "electronics", "furniture", "photo + video", "tickets"]
 
 categories.each do |category|
   Category.create!(name: category)
   end
+
+50.times do 
+  Article.create!(title: Faker::Commerce.product_name, 
+                 price: Faker::Commerce.price, 
+                 email: Faker::Internet.email,
+                 description: Faker::Hipster.paragraph,
+                 category_id: [*1..8].sample)
+end

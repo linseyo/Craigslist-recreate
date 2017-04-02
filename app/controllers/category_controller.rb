30a70d46494d92aee266c7d9d8ff9956378d15f2
Jordan Levine <jordan@strofina.com>
2017-04-02 12:58:19 -0700

    remove debug messages

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -5,14 +5,6 @@
 
   get '/categories/:category_id/articles' do
     @category=Category.find(params[:category_id])
-    puts "CATEGORY to FOLLOW"
-    p @category
-    # @articles = Article.where(category_id: params[:category_id])
     @articles = @category.articles
- 
-    puts "Articles length is #{@articles.length}"
-    puts "Articles count is #{@articles.count}"
-    puts "ARTICLES TO FOLLOW:"
-    p @articles
     erb :"../views/articles/show"    
   end

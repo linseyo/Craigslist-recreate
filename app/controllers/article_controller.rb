5d09205aa82509da5189d1e04358033cf0e41521
Jordan Levine <jordan@strofina.com>
2017-04-02 17:51:46 -0700

    Refactor save functionality

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -28,19 +28,15 @@ end
 
 # save article
 post '/categories/:category_id/articles' do
-  "about to save article!  need code here"
   # make new article record and shovel into category
   this_category=Category.find(params[:category_id])
   new_article=Article.new(params['article'])
   this_category.articles << new_article
   this_category.save
-  "just saved to database!"
   # get article id of just-saved article
   new_article_id=new_article.id
 
   # generate secret url and display to user
-  secret_url = 'localhost:9393/categories/#{params[:category_id]}/articles/#{new_article_id}'
-  "The secret URL is: #{secret_url}"
-
-  <a href="/" >Return to CraigsList, Junior, Homepage!></a>
+  @secret_url = "localhost:9393/categories/#{params[:category_id]}/articles/#{new_article_id}"
+  erb :'../views/articles/save'
 end
